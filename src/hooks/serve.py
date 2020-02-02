635cabce9cc6be48bd797f69ff378bef4edcb8da
donicrosby <donicrosby1995@gmail.com>
2020-02-02 16:28:45 -0500

    Fixed missing encoding for writing bytes out

--- src/hooks/serve.py
+++ src/hooks/serve.py
@@ -136,7 +136,7 @@ class Controller(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(response))
+            self.wfile.write(json.dumps(response).encode('utf-8'))
         else:
             self.send_error(500)
 
